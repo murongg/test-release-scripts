5ffd9ee50e0d8017ae9947cba563a70009adf978
MuRong <hi@mrong.me>
2024-02-25 14:39:54 +0800

    update

--- scripts.config.ts
+++ scripts.config.ts
@@ -2,7 +2,37 @@ import { defineConfig } from '@hyperoracle/release-scripts'
 
 export default defineConfig({
   entries: [
-    'src/index',
+    {
+      input: 'src/index',
+      distDir: './dist',
+      name: 'index',
+      build: {
+        declaration: true,
+        clean: true,
+        rollup: {
+          emitCJS: true,
+          esbuild: {
+            define: {
+              __BROWSER__: 'false',
+            }
+          }
+        },
+      }
+    },
+    {
+      input: 'src/index',
+      outDir: './dist2',
+      name: 'browser',
+      build: {
+        rollup: {
+          esbuild: {
+            define: {
+              __BROWSER__: 'true',
+            }
+          }
+        },
+      }
+    }
   ],
   build: {
     declaration: true,
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export const one = 1
 export const two = 2
+
+export const testESbuild = () => {
+  if(__BROWSER__) {
+    console.log('__BROWSER__')
+  }
+}
